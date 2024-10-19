dbf4a1e2009ed82eafb00bc84810f78a4b21d807
Itserge1 <sergecodefirst@gmail.com>
2024-10-19 12:12:34 -0400

    add comment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 8000;
 
 const cors = require('cors');
 app.use(cors());
-
+//  add comment
 app.get("/api", (req, res) => {
     return res.json({name: "Albert", img:"https://as2.ftcdn.net/v2/jpg/01/22/94/81/1000_F_122948102_mK5rrCUW0U18vikIpb6AX84skMqBuFnW.jpg"})
 })
